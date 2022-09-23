3b4734500db360c8b70efa20ba1b4d9ba03311b7
Milen961 <theheller178@gmail.com>
2022-09-23 12:35:17 +0300

    Adding middlewares/createController

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const hbs = require('express-handlebars').create({
 });
 const homeController = require('./controllers/homeController')
 const catalogController = require('./controllers/catalogController')
+const createController = require('./controllers/createController')
 const defaultController = require('./controllers/defaultController');
+const deafaultTitle = require('./middlewares/deafaultTitle');
 
 const app = express();
 app.engine('.hbs', hbs.engine);
@@ -13,8 +15,11 @@ app.set('view engine', '.hbs')
 app.use(express.urlencoded({ extended:true}));
 app.use('/static', express.static('static'));
 
+app.use(deafaultTitle('Softuni Accomodation'))
+
 app.use(homeController)
 app.use('/catalog', catalogController)
+app.use('/create', createController)
 
 
 app.all('*', defaultController);
