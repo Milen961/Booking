afbe5220d21f0d587b98d07bdf22777c9e38606c
Milen961 <theheller178@gmail.com>
2022-10-09 15:54:53 +0300

    creating ( unfinished) register logic and connecting it to the DB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const { login } = require('../services/authService');
+const { login, register } = require('../services/authService');
 
 const authController = require('express').Router();
 
@@ -10,9 +10,24 @@ authController.get('/login', (req, res) => {
 
 authController.post('/login', async (req, res) =>{
     const result = await login(req.body.username, req.body.password);
-    const token = req.signJwt(result);
-    res.cookie('jwt', token);
+    attachToken(req, res, result)
     res.redirect('/')
 });
+authController.get('/register', (req, res) => {
+    res.render('register', {
+      title: 'Register'
+    });
+  });
+
+  authController.post('/register', async (req, res) =>{
+     const result = await register(req.body.username, req.body.password);
+     attachToken(req, res, result);
+    res.redirect('/')
+});
+
+function attachToken(req, res, data){
+    const token = req.signJwt(data);
+    res.cookie('jwt', token, { maxAge: 14400000 });
+}
 
 module.exports = authController;
--- services/authService.js
+++ services/authService.js
@@ -1,3 +1,28 @@
+ const bcrypt = require('bcrypt')
+ const User = require('../models/User')
+
+ async function register(username, password){
+    // check if username is taken
+   const existing = await User.findOne({username: { $regex: new RegExp(username), $options: 'i'} });
+if (existing){
+   throw new Error('Username is taken') 
+}
+   // hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+   
+   // create and save user
+   const user = await User.create({
+        username,
+        hashedPassword
+    });
+   // retunr user data 
+    return {
+        username,
+        roles: user.roles
+    }
+  }
+ 
+
 async function login(username, password){
   return new Promise((res, rej) => {
     if (username.toLowerCase() == 'peter' && password == '123456') {
@@ -12,5 +37,6 @@ async function login(username, password){
   });
 }
 module.exports = {
+    register,
     login
 };
